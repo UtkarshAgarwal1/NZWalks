4ff6a620810e66eb90379828a9a1aaf7b26b9b28
UtkarshAgarwal1 <utkarshagarwal.9095@gmail.com>
2022-12-24 06:13:23 +0100

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Model/Domain/Walks.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Model.Domain
+{
+    public class Walks
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigration Property
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
